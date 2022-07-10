29ba0e9f4b889b3942ab84839cdda4ebdb3bfeaa
its my name <hello@gmail.com>
2022-07-10 17:03:55 +0530

    bhargavi code commits

---
+++ file.js
@@ -0,0 +1,11 @@
+function message(){
+    alert("submitted succesfully");
+}
+function message2(){
+    if ((document.getElementById("check")).checked) {
+        alert("your password was saved successfully");
+    }
+    else{
+        alert("Your password was not saved")
+    }
+}
